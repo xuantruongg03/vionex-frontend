d438d60121ca01223b8f29d48d7a5c4de6928b9d
xuantruongg003 <lexuantruong098@gmail.com>
2025-08-21 21:15:18 +0700

    remove secret

--- vite.config.ts
+++ vite.config.ts
@@ -9,12 +9,11 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 5173,
-    https: {
-      key: fs.readFileSync("secrets/private-key.pem"),
-      cert: fs.readFileSync("secrets/public-certificate.pem"),
-    },
+    // https: {
+    //   key: fs.readFileSync("secrets/private-key.pem"),
+    //   cert: fs.readFileSync("secrets/public-certificate.pem"),
+    // },
     allowedHosts: [
-      "8fb0-42-1-85-88.ngrok-free.app", 
       "localhost", 
     ],
   },
