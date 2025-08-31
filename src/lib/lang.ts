a05caaddcb908f6d7ef4771799b565cfb8778209
xuantruongg003 <lexuantruong098@gmail.com>
2025-08-31 19:02:20 +0700

    update multi lan for cabin translate

--- src/lib/lang.ts
+++ src/lib/lang.ts
@@ -1,4 +1,5 @@
 export const SUPPORTED_LANGUAGES_TRANSLATION = [
     { code: "vi", name: "Tiếng Việt" },
-    { code: "en", name: "English" },
+    { code: "en", name: "Tiếng Anh" },
+    { code: "lo", name: "Tiếng Lào" },
 ];
